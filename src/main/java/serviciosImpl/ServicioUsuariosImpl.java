be311f09e8aaac11e7d1dc768d5018634d268a41
USUARIO <diegovalerahernandez@gmail.com>
2026-07-04 15:52:49 +0200

    añadido archivo a setting org.eclipse.buildship.core.prefs, cambiado puerto de comunicaciones a 3038

--- src/main/java/serviciosImpl/ServicioUsuariosImpl.java
+++ src/main/java/serviciosImpl/ServicioUsuariosImpl.java
@@ -1,29 +1,28 @@
-package serviciosImpl;
+package serviciosImpl; 
 
 import java.util.List;
 import java.util.Map;
+import javax.annotation.Resource; // <-- CORREGIDO: Vuelve a javax para Tomcat 9
+import modelo.Usuario;
+import servicios.ServicioUsuarios;
 
-import javax.annotation.Resource;
-
-import org.hibernate.Criteria;
-import org.hibernate.SQLQuery;
-import org.hibernate.SessionFactory;
-import org.hibernate.criterion.Restrictions;
-import org.hibernate.transform.AliasToEntityMapResultTransformer;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import constantesSQL.ConstantesSQL;
-import modelo.Categoria;
-import modelo.Usuario;
-import servicios.ServicioUsuarios;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
+import org.hibernate.criterion.Restrictions;
+import org.hibernate.transform.AliasToEntityMapResultTransformer;
 
 @Service
 @Transactional
-public class ServicioUsuariosImpl implements ServicioUsuarios {
-	
-	//@Resource asigna la bean de la id indicada, en este caso fff
-	//a la variable a continuación definida
+public class ServicioUsuariosImpl implements ServicioUsuarios { 
+
+	// @Resource asigna la bean de la id indicada, en este caso fff
+	// a la variable a continuaciÃ³n definida
 	@Resource(name = "fff")
 	private SessionFactory sessionFactory; 
 
@@ -32,18 +31,20 @@ public class ServicioUsuariosImpl implements ServicioUsuarios {
 		sessionFactory.getCurrentSession().save(u);		
 	}
 
+	@SuppressWarnings({ "deprecation", "unchecked" })
 	@Override
 	public boolean comprobarEmail(String email) {
 		Criteria c = sessionFactory.getCurrentSession().createCriteria(Usuario.class);
 		c.add(Restrictions.eq("email", email));
 		if (c.uniqueResult() == null) {
 			return false;
-		}else {
+		} else {
 			return true;
 		}
-	
 	}
 
+	// Con esta anotacion conseguimos que java deje usar metodos antiguos de hibernate como criteria
+	@SuppressWarnings({ "deprecation", "unchecked" })
 	@Override
 	public Usuario obtenerUsuarioPorEmailYpass(String email, String pass) {
 		Criteria c = sessionFactory.getCurrentSession().createCriteria(Usuario.class);
@@ -52,11 +53,13 @@ public class ServicioUsuariosImpl implements ServicioUsuarios {
 		return (Usuario) c.uniqueResult();
 	}
 
+	@SuppressWarnings({ "deprecation", "unchecked" })
 	@Override
 	public List<Usuario> obtenerUsuarios() {
 		return sessionFactory.getCurrentSession().createCriteria(Usuario.class).list();
 	}
 
+	@SuppressWarnings({ "deprecation", "unchecked" })
 	@Override
 	public Map<String, Object> obtenerUsuarioPorId(int id) {
 		SQLQuery query = sessionFactory.getCurrentSession().createSQLQuery(ConstantesSQL.SQL_OBTENER_USUARIO_POR_ID);
@@ -68,15 +71,23 @@ public class ServicioUsuariosImpl implements ServicioUsuarios {
 	@Override
 	public void darBajaUsuario(int id) {
 		Usuario u = (Usuario) sessionFactory.getCurrentSession().get(Usuario.class, id);
-		u.setAlta(false);
-		sessionFactory.getCurrentSession().update(u);
+		if (u != null) {
+			u.setAlta(false);
+			sessionFactory.getCurrentSession().update(u);
+		}
 	}
 
 	@Override
 	public void darAltaUsuario(int id) {
 		Usuario u = (Usuario) sessionFactory.getCurrentSession().get(Usuario.class, id);
-		u.setAlta(true);
-		sessionFactory.getCurrentSession().update(u);
+		if (u != null) {
+			u.setAlta(true);
+			sessionFactory.getCurrentSession().update(u);
+		}
 	}
 
 }
+ 
+	
+
+ 
